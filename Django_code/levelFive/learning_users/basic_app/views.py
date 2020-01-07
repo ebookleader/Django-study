c1eebc111db46e60f14f4025fb38dbf429db96ef
JEunLe <hemslje@naver.com>
2020-01-08 00:31:16 +0900

    commit

--- Django_code/levelFive/learning_users/basic_app/views.py
+++ Django_code/levelFive/learning_users/basic_app/views.py
@@ -15,14 +15,14 @@ def user_login(request):
         # First get the username and password supplied
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         # Django's built-in authentication function:
         user = authenticate(username=username, password=password)
 
         # If we have a user
         if user:
             #Check it the account is active
-            if user.is_active:
+
+            if user.is_activate:
                 # Log the user in.
                 login(request, user)
                 # Send the user back to some page.
